0559b732ae2a5c68dd54fcff358b23c0ee177f92
avipinto <pinto.avi@gmail.com>
2017-01-20 16:51:26 +0200

    real first commit

---
+++ Assets/Wave.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave : MonoBehaviour {
+
+	public Disk[] disks;
+
+	void Awake () {
+		disks = FindObjectsOfType<Disk> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		//disk = GameObject.FindObjectOfType<Disk> ();
+
+		MakeWaves ();
+
+		StartCoroutine (DrawWave ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void MakeWaves () {
+		foreach (var disk in disks) {
+			Vector2 v = (disk.transform.position - transform.position);
+			var directon = v.normalized;
+			var force = 100 / v.magnitude;
+			disk.Hit (directon * force);
+		}
+	}
+
+	IEnumerator DrawWave(){
+		for (int i = 0; i < 5; i++) {
+			this.transform.localScale *= 1.5f;
+			yield return new WaitForSeconds (0.05f);
+		}
+		Destroy (gameObject);
+	}
+}

e847ba39a1768cd4426b1dd6d603c9b07a22c830
avipinto <pinto.avi@gmail.com>
2017-01-20 16:33:15 +0200

    first commit

---
+++ Assets/Disk.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disk : MonoBehaviour {
+
+	Rigidbody2D body;
+
+	// Use this for initialization
+	void Awake () {
+		//Vector3 direction = transform.position - Vector3.left * 5;
+		body = GetComponent<Rigidbody2D> ();
+
+		//body.AddForceAtPosition (direction.normalized, transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Hit(Vector2 force) {
+		body.AddForce (force);
+	}
+}
